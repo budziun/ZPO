ef3d0cfcc9c85d53a97e4cfccf9a6cbf8bb1d182
Jakub Budzich <115219579+budziun@users.noreply.github.com>
2026-02-25 17:39:17 +0100

    zpo1 - 5 zadan w domu zrobione

--- zpo1.py
+++ zpo1.py
@@ -95,11 +95,146 @@ class Product:
         if self.price <= 0:
             raise ValueError(f"Cena musi być większa od zera {self.price}")
 
-produkt = Product(name="produkt1", price=100.00)
-print(produkt.category)
-produkt2 = Product(name="produkt2", price=-100.00)
+
+produkt2 = Product(name="produkt2", price=100.00)
 print(produkt2.price)
 
 #ZAD5
+# Utworzyć klasę Car z atrybutami brand, model i year. Następnie dodać metodę is_classic(),
+# która zwróci True, jeśli samochód ma ponad 25 lat.
+
+class Car:
+    brand: str
+    model: str
+    year: int
+
+    def __init__(self, brand : str, model : str, year : int):
+        self.brand = brand
+        self.model = model
+        self.year = year
+    def isClassic(self)-> bool:
+        if 2026-self.year > 25:
+            return True
+        else:
+            return False
 
 
+autko_stare = Car("Fiat", "126p", 1990)
+print(f"1990 auto: ",autko_stare.isClassic())
+autko_nowe = Car("Tesla","Y", 2020)
+print(f"2020 auto: ",autko_nowe.isClassic())
+
+#ZAD6
+#Stworzyć klasy ElectricVehicle oraz GasolineVehicle, które mają metodę fuel_type(),
+# zwracającą odpowiednio "electric" i "gasoline". Następnie utworzyć klasę HybridCar,
+# która dziedziczy po obu i nadpisuje metodę fuel_type(), aby zwracała "hybrid".
+
+class ElectricVehicle:
+    def fuel_type(self)-> str:
+        return "electric"
+
+class GasolineVehicle:
+    def fuel_type(self)-> str:
+        return "gasoline"
+
+class HybridCar(ElectricVehicle, GasolineVehicle):
+    def fuel_type(self)-> str:
+        return "hybrid"
+
+elektryk = ElectricVehicle()
+gazolina = GasolineVehicle()
+hybryda = HybridCar()
+print("Elektryk jeździ na: ",elektryk.fuel_type())
+print("Gaziutek jeździ na: ",gazolina.fuel_type())
+print("Hybryda jeździ na: ",hybryda.fuel_type())
+
+#ZAD7
+#Utworzyć klasę Person z metodą introduce(), zwracającą "I am a person".
+# Następnie stworzyć klasy Worker i Student, które dziedziczą po Person i zmieniają tę metodę na "I am a worker"
+# oraz "I am a student". Następnie utworzyć klasę WorkingStudent, która dziedziczy zarówno po Worker, jak i Student,
+# i sprawdź, jak Python rozwiąże konflikt metod.
+
+class Person:
+    name: str
+    lastname: str
+
+    def __init__(self, name: str, lastname: str) -> None:
+        self.name = name
+        self.lastname = lastname
+    def introduce(self) -> str:
+        return "I am a person"
+
+class Worker(Person):
+    def introduce(self) -> str:
+        return "I am a worker"
+
+class Student(Person):
+    def introduce(self) -> str:
+        return "I am a student"
+
+class WorkingStudent(Worker, Student):
+    pass
+
+pracujacy_student = WorkingStudent("Jakub", "Budzich")
+print(pracujacy_student.introduce())
+# Zwróciło że jest Worker
+
+class WorkingStudent2(Student, Worker):
+    pass
+
+pracujacy_student2 = WorkingStudent2("Jakub", "Budzich")
+print(pracujacy_student2.introduce())
+# Zwróciło Student
+# Czyli w python pierwsze dziediczenie jest ważniejsze i liczy się po jakiej klasie wpierw dziediczymy.
+
+#ZAD8
+# Utworzyć klasy Animal i Pet. Klasa Animal powinna mieć metodę make_sound(),
+# zwracającą "Some sound", a Pet powinna mieć metodę is_domestic(), zwracającą True.
+# Następnie utworzyć klasę Dog, dziedziczącą po obu, i dostosować metody tak, aby pasowały do psa.
+
+class Animal:
+    def make_sound(self) -> str:
+        return "Some sound"
+
+class Pet:
+    def is_domestic(self) -> bool:
+        return True
+
+class Dog(Animal, Pet):
+    def make_sound(self) -> str:
+        return "Hau Hau Hau"
+
+burek = Dog()
+print(burek.make_sound())
+print(burek.is_domestic())
+
+#ZAD9
+# Zaimplementować klasy FlyingVehicle i WaterVehicle, które mają metody move(),
+# zwracające odpowiednio "I fly" oraz "I sail". Następnie stworzyć klasę AmphibiousVehicle,
+# która łączy obie i pozwala na wybór trybu działania.
+
+class FlyingVehicle:
+    def move(self)-> str:
+        return "I fly"
+
+class WaterVehicle:
+    def move(self)-> str:
+        return "I sail"
+
+class AmphibiousVehicle(FlyingVehicle, WaterVehicle):
+    mode: str
+
+    def __init__(self, mode: str) -> None:
+        self.mode = mode
+
+    def move(self) -> str:
+        if self.mode == "Water":
+            return WaterVehicle.move(self)
+        elif self.mode == "Air":
+            return FlyingVehicle.move(self)
+
+
+samolot = AmphibiousVehicle("Air")
+lodka = AmphibiousVehicle("Water")
+print(samolot.move())
+print(lodka.move())
